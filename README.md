054b0dd17a52fc580acbc62867bba0e6916246dd
Bhavya <bhavya@Bhavyas-MBP.ht.home>
2022-07-24 12:09:03 -0400

    improved documentation, added end point for avg delays

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# AIRLINES DELAY APP
+# AIRLINES DELAY API
 
-## Steps
+## Trying out FastAPI for Python. Used with SQlLITE local database. This is a testing project not a complete app.
+
+## Usage
 
 1. Download Kaggle dataset to root folder
    `https://www.kaggle.com/datasets/giovamata/airlinedelaycauses`
 2. Run all the cells from `data.ipynb`
    - this will generate file `good_data.csv`
-3. Run `load_database.py` to poulste the database
+3. Run `load_database.py` to populate the database
 4. App is ready to run using `uvicorn app:app`
+
+### Accomplished
+
+- Fast API for basic CRUD operations
+- Connection to a local database
+- SQL querying and filtering the database
+- Aggregate functions in SQLAlchemy
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
 from starlette.responses import RedirectResponse
 from typing import List
 
@@ -33,6 +34,11 @@ def main():
 
 @app.get("/records/{carrier}-{flight}", response_model=schemas.Record)
 def show_records(carrier: str, flight:int, db: Session = Depends(get_db)):
-    records = db.query(models.Record).filter(models.Record.unique_carrier == carrier and models.Record.flight_num == flight).first()
-    print(db.query(models.Record).filter(models.Record.unique_carrier == carrier and models.Record.flight_num == flight).count())
+    records = db.query(models.Record).filter(models.Record.unique_carrier == carrier).filter( models.Record.flight_num == flight).first()
+    # print(db.query(models.Record).filter(models.Record.unique_carrier == carrier and models.Record.flight_num == flight).count())
     return records
+
+@app.get("/delays/{carrier}-{flight}-{day}")
+def show_delays(carrier: str, flight:int,day:int, db: Session = Depends(get_db)):
+    q = db.query(models.Record.unique_carrier, models.Record.flight_num, models.Record.day_of_week, func.avg(models.Record.dep_delay).label("avg_dep_delay"), func.avg(models.Record.arr_delay).label("avg_arr_delay")).group_by(models.Record.unique_carrier, models.Record.flight_num, models.Record.day_of_week).filter(models.Record.flight_num == flight).filter(models.Record.unique_carrier == carrier).filter(models.Record.day_of_week == day)
+    return q.first()
